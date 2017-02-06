fe2ec0b2ed3c43ca3fffeeb0ed1cfc631fdf7f4c
youngnac <youngnac@gmail.com>
2017-02-06 16:09:11 +0900

    create polls model, register admin

--- django_app/mysite/settings.py
+++ django_app/mysite/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig'
+    'django_extensions',
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
